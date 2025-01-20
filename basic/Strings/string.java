9fd6b100a83907346888037ddcd46a829c0af40e
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2025-01-20 21:47:50 +0530

    day 8

--- basic/Strings/string.java
+++ basic/Strings/string.java
@@ -29,6 +29,98 @@ public class string {
 
 
 
+//     static void index(String s){
+//         int n=s.length();
+    
+//     for(int i=0;i<n;i++){
+//         if(s.charAt(i)=='a'){
+//             System.out.println(s.indexOf(s.charAt(i)));
+//         }
+//     }
+//     }
+// public static void main(String[] args) {
+    
+    
+//     Scanner s=new Scanner(System.in);
+//     String st=s.nextLine();
+    
+//     // String t=s.nextLine();
+
+//     index(st);
+
+// }
+
+
+                                        // COUNT (A)
+
+
+
+// static void index(String s){
+//     int n=s.length();
+
+// for(int i=0;i<n;i++){
+//     if(s.charAt(i)=='a'){
+//         System.out.println(s.indexOf(s.charAt(i)));
+//     }
+// }
+// }
+// public static void main(String[] args) {
+
+
+// Scanner s=new Scanner(System.in);
+// String st=s.nextLine();
+
+// // String t=s.nextLine();
+
+// index(st);
+
+// }
+
+
+
+                                // LOWERCASE AND UPPERCASE
+
+
+// static void index(String s){
+    
+//     String u=s.toUpperCase();
+//     System.out.println(u);
+//     String l=s.toLowerCase();
+//     System.out.println(l);
+// }
+
+// public static void main(String[] args) {
+
+
+// Scanner s=new Scanner(System.in);
+// String st=s.nextLine();
+
+
+// index(st);
+
+// }      
+
+
+                                    // SUBSTRING
+
+
+// static void index(String s){
+    
+//     System.out.println(s.substring(2));
+// }
+
+// public static void main(String[] args) {
+
+
+// Scanner s=new Scanner(System.in);
+// String st=s.nextLine();
+
+
+// index(st);
+
+// }    
+
+
 
 
 
