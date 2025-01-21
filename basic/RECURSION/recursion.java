58c00e3ecaf2d1495f91b4fd078e7cad53c73f1d
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2025-01-21 19:27:21 +0530

    day 9

--- basic/RECURSION/recursion.java
+++ basic/RECURSION/recursion.java
@@ -4,17 +4,31 @@ public class recursion {
 
                                     // FACTORIAL
 
-                                    
-public static int fac(int n){
-        if(n==1) return 1;
-        int ans=n*fac(n-1);
-        return ans;
-   }
+
+// public static int fac(int n){
+//         if(n==1) return 1;
+//         int ans=n*fac(n-1);
+//         return ans;
+//    }
+//    public static void main(String[] args) {
+//         Scanner s=new Scanner(System.in);
+//         int n=s.nextInt();
+//         System.out.println(fac(n));
+//    }
+
+
+
+
+    public static int num(int n){
+        if(n==0) return 0;
+        return a;
+        num(n-1);
+    }
    public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        int n=s.nextInt();
-        System.out.println(fac(n));
-   }
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    System.out.println(num(n));
+}
 
 
 
---
+++ basic/SORTING/sorting.java
@@ -0,0 +1,61 @@
+package SORTING;
+
+import java.util.Scanner;
+
+public class sorting {
+
+ 
+    
+
+
+// public static void check(int[] num){
+//     int l=0;
+//     int i=0;
+//     int h=num.length-1;
+//     int ans=0;
+//     while(i<=h){
+//         if(num[l]>num[i]){
+//             ans=1;
+            
+//             break;
+//         }i++;
+//     }
+
+//         if(ans==0) System.out.println("true");
+//         else System.out.println("false");
+
+// }    
+// public static void main(String[] args) {
+//     Scanner s=new Scanner(System.in);
+//     int n=s.nextInt();
+//     int[] arr=new int[n];
+//     for(int i=0;i<n;i++){
+//         arr[i]=s.nextInt();
+//     }
+//     check(arr);
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+}
