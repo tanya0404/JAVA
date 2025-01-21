733967de87f50d0d5f05884e18bc38a3ed7af6df
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2025-01-21 13:42:49 +0530

    Day 9

---
+++ basic/RECURSION/recursion.java
@@ -0,0 +1,43 @@
+package RECURSION;
+import java.util.*;
+public class recursion {
+
+                                    // FACTORIAL
+
+                                    
+public static int fac(int n){
+        if(n==1) return 1;
+        int ans=n*fac(n-1);
+        return ans;
+   }
+   public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        System.out.println(fac(n));
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
