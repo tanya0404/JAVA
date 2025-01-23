22bf193f4f3648a7d18bd967604609ecfb7dd753
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2025-01-23 21:43:16 +0530

    Day 11

--- basic/BINARYSCH/binary.java
+++ basic/BINARYSCH/binary.java
@@ -4,53 +4,42 @@ import java.util.Scanner;
 
 public class binary {
     
-// static void bs(int[] num,int k){
-//     int l=0;int h=num.length-1;
-//     while(l<=h){
-//         int mid =l+(h-1)/2;
-//         if(k==mid){
-//             System.out.println(k);
-//             break;
-//         }
-//         else if(k>mid){
-//             l=mid+1;
-            
-//         }
-//         else if(k<mid){
-//             h=mid-1;
-            
-//         }
-//         }
-// }
-// public static void main(String[] args) {
-//     Scanner s=new Scanner(System.in);
-//     int n=s.nextInt();
-//     int[] arr=new int[n];
-
-//     for(int i=0;i<n;i++){
-//         arr[i]=s.nextInt();
-//     }
-
-//     int k=s.nextInt();
-//     bs(arr,k);
-// }
-
-
-
-
-static void lubound(int[] num,int k){
-    int n=num.length;
-    int l=0;
-    int h=n-1;
-    
+static void bs(int[] num,int k){
+    int l=0;int h=num.length-1;
     while(l<=h){
-        
-        int mid=l+(h-l)/2;
-        if(k>=mid){
-
+        int mid =l+(h-l)/2;
+        if(k==num[mid]){
+            System.out.println(mid);
+            break;
         }
-    }
+        else if(k>num[mid]){
+            l=mid+1;
+            
+        }
+        else if(k<num[mid]){
+            h=mid-1;
+            
+        }
+        }
+        
 }
+public static void main(String[] args) {
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    int[] arr=new int[n];
+
+    for(int i=0;i<n;i++){
+        arr[i]=s.nextInt();
+    }
+
+    int k=s.nextInt();
+    bs(arr,k);
+}
+
+
+
+
+
 
 
 
